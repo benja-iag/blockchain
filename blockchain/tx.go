82c75952e68ffabab7ff230de3cbcd52fd258d0e
Benjamin <62310877+benja-iag@users.noreply.github.com>
2023-10-11 01:12:41 +0000

    feat: signature

--- blockchain/tx.go
+++ blockchain/tx.go
@@ -1,20 +1,42 @@
 package blockchain
 
+import (
+	"blockchain1/wallet"
+	"bytes"
+)
+
 type TxOutput struct {
-	Value  int
-	PubKey string
+	Value         int
+	PublicKeyHash []byte
 }
 
 type TxInput struct {
-	ID  []byte
-	Out int
-	Sig string
+	ID        []byte
+	Out       int
+	Signature []byte
+	PublicKey []byte
 }
 
-func (in *TxInput) CanUnlock(data string) bool {
-	return in.Sig == data
+func NewTxOutput(value int, address string) *TxOutput {
+	txo := &TxOutput{value, nil}
+	txo.Lock([]byte(address))
+
+	return txo
 }
 
-func (out *TxOutput) CanBeUnlocked(data string) bool {
-	return out.PubKey == data
+func (in *TxInput) UsesKey(publicKeyHash []byte) bool {
+	lockingHash := wallet.PublicKeyHash(in.PublicKey)
+
+	return bytes.Equal(lockingHash, publicKeyHash)
+}
+func (out *TxOutput) Lock(address []byte) {
+	publicKeyHash := wallet.Base56Decode(address)
+	publicKeyHash = publicKeyHash[1 : len(publicKeyHash)-4]
+
+	out.PublicKeyHash = publicKeyHash
+}
+
+func (out *TxOutput) IsLockedWithKey(publicKeyHash []byte) bool {
+
+	return bytes.Equal(out.PublicKeyHash, publicKeyHash)
 }
